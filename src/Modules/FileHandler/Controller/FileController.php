993c80a1cbcd0d430d8273cfaf81596b6fd4c7b1
sugandha <sugandha.trivedi@impelsys.com>
2020-02-05 22:58:57 +0530

    Initial Commit

---
+++ src/Modules/FileHandler/Controller/FileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\FileHandler\Controller;
+
+class FileController {
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+
+}
---
+++ tests/test.php
@@ -0,0 +1,8 @@
+<?php
+ 
+require_once __DIR__ . '/../vendor/autoload.php';
+ 
+use App\Modules\FileHandler\Controller\FileController;
+ 
+ 
+echo FileController::world();
