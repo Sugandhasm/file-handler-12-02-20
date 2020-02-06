a87023d2283fdf2aa41f8e59fac102d4164a21ad
sugandha <sugandha.trivedi@impelsys.com>
2020-02-06 18:52:40 +0530

    change #31

--- src/Modules/FileHandler/Controller/FileController.php
+++ src/Modules/FileHandler/Controller/FileController.php
@@ -3,6 +3,10 @@
 namespace App\Modules\FileHandler\Controller;
 
 class FileController {
+    
+    /**
+     * @Route("/world", name="world_list")
+     */
     public static function world()
     {
         return 'Hello World, Composer!';
